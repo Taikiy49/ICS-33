c58cb630e0a5cdac8cb4c35a8f9c2d3a486b1b5f
Taiki Yamashita <toyamash@uci.edu>
2023-12-08 18:35:28 -0800

    Finished week 1

--- Final Review/week1.py
+++ Final Review/week1.py
@@ -101,8 +101,8 @@ test_positional(1, 2, c=3, d=5)
 def subtract(a, b, c, /, *, d=0): #  NOTE / must go before *...
     return d - (a + b + c)
 
-subtract(1, 2, 3)
-subtract(1, 2, 3, d=8)
+# subtract(1, 2, 3)
+# subtract(1, 2, 3, d=8)
 """NOW THATS PRETTY COOL!"""
 
 # try make something else again! Try implement everything you've learned in week 1!!
@@ -112,9 +112,97 @@ def everything(a, b, /, *, c, **kwargs):
             print(key, value)
     print(c)
 
-everything(1, 2, a=3, b=5, c=6, this='is cool')
+# everything(1, 2, a=3, b=5, c=6, this='is cool')
 
 """NOTE CONTEXT MANAGERS NOTE"""
+
+def try_this(x):
+    try:
+        inp = int(x)
+        print(inp)
+    except ValueError:
+        print('oh no a value error...')
+    finally:
+        print('prints anyways!')
+
+"""so when you have a try and except, if you have a finally block,
+    that finally block will always run in the end!"""
+# try_this('abc')
+
+def read_file(file):
+    with open(file, 'r') as infile:
+        print(infile.read())
+
+# read_file('../ICS-33/Final Review/taiki.txt')
+
+"""What does return self do?"""
+class Something:
+    def __init__(self, age):
+        self._age = age
+    def do_something(self):
+        return self
+# SO the one above basically returns the object itself and you can access everything about it using
+# .{whatever}. interesting...
+class SomethingElse:
+    def __init__(self, age):
+        self._age = age
+    def do_something_else(self):
+        return self._age
+
+# print(Something(15).do_something()._age)
+# print(SomethingElse(15).do_something_else())
+
+
+
+"""Now... The real deal for Context Managers
+Yes... They have an enter and exit"""
+
+
+"""The purpose of context managers are usually to start it up, set it up for you.
+    Then you do whatever you need to do... 
+    Then the context manager comes into play again and cleans everything up for it to be ready."""
+class MyContextManager:
+    def __init__(self, name, age):
+        print('Initializing...')
+        self._name = name
+        self._age = age
+    def __enter__(self):
+        print('Entering...')
+        return self
+    def __exit__(self, exc_type, exc_value, exc_traceback):
+        """For the exit method, we should have the type, value, and traceback as parameters.
+            1) the type will specify the type of exception...
+            2) the value will specificy the error message...
+            3) the traceback will specify the traceback... LOL
+        """
+        if exc_type is None: # meaning there is nothing that happened to cause some chaos...
+                             # it is defaulted with None btw...
+            print('Exiting with no chaos :)')
+        else:
+            print('Exiting because of chaos :(')
+            print(exc_type.__name__)
+
+        
+
+# print(MyContextManager('taiki', 10)) <- THIS IS INVALID BECAUSE FOR AN ENTER METHOD TO RUN,
+# IT HAS TO BE IN A WITH STATEMENT SO...
+try:
+    with MyContextManager('taiki', 15) as manager:
+        print('We are now inside the block!')
+        if manager._age < 19:
+            raise ValueError()
+except ValueError:
+    pass
+
+"""So what I have is a context manager that takes the name and age.
+    What I can do with a context manager is to first set valid class arguments.
+    Now, if I have any sort of error inside the with statement, the context manager will catch it,
+    and exit with chaos. If there are no errors inside the with statement, there will be no chaos.
+    This will be very useful in like cases with what I did where once I stored the age in the
+    class argument, I checked if my age was valid by checking if it was less than 19. If it was less
+    than 19, it will have raised a ValueError and the context manager would have printed what it
+    needs to print in the exit method.
+"""
 
 
 
